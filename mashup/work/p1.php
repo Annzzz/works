6e68a4d0d520fd9f2b13bd810444b0e0a667698a
Anju Baby <you@example.com>
2017-09-22 11:33:37 +0530

    my works

---
+++ mashup/work/p1.php
@@ -0,0 +1,7 @@
+<?php
+for ($row=1; $row <=5 ; $row++) 
+{ 
+	echo "* \n";
+	
+}
+?>
---
+++ mashup/work/p2.php
@@ -0,0 +1,11 @@
+<?php
+for ($row=1; $row <=5; $row++) 
+{ 
+	for ($col=1; $col <=5; $col++) 
+	{ 
+		echo "*";
+	}
+	echo "\n";
+}
+
+?>
